1b1e41d5a8de55692e01f48ff1e8992dda758b7d
Jesus Ivan <jim15mr@gmail.com>
2016-09-28 21:00:29 -0700

    se creo el programa

---
+++ JavaApplication1.java
@@ -0,0 +1,12 @@
+
+
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+       
+        int s1=5,s2=5;
+       int suma =s1+s2;
+        System.out.println("la suma es:"+ suma);
+    }
+    
+}
